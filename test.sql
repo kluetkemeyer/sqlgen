04a68dc5cf2e77d31ebffa8eac8040e06f2e5c45
Kilian Lütkemeyer <kilian@luetkemeyer.com>
2013-10-25 13:22:45 +0200

    adjusted default test file for new input

--- test.sql
+++ test.sql
@@ -1,19 +1,21 @@
-SELECT 
-	select1.asdf IN (15, 12, 2), 
-	5 AS fuenf, 
-	tab.set1.k[15] AS indirectAccess,
-	"Srtin Hello" AS const_string
-FROM
-	tabular1 AS tab,
-	(SELECT
-		"blub",
-		x1.id AS id
-	FROM
-		tabular2 AS x2
-	) AS select1,
-	tabular3
-JOIN adf AS asdf
-LEFT OUTER JOIN asedfer ON 42 = 23
-WHERE
-	tabular1.id = tabular3.id
-	AND select1.id IN (SELECT CONCAT(1, ""))
+project "TT-DB"
+
+define enumeration Gender {
+	MALE,
+	FEMALE
+}
+
+define private statement _user_categories(userId) {
+	SELECT		user.id								AS	userId,
+				NULL								AS	personId,
+				category.id							AS	categoryId
+	FROM		user__user							AS	user
+	JOIN		user__category						AS	category
+		ON		user.id = category.id
+	WHERE		user.id = userId
+}
+
+define public statement getUserCategories(userId) {
+	_user_categories(userId)
+}
+	
